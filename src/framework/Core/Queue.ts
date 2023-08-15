4077c8242c5cac7e3242a8adccc1377388a040c8
Rastislav Madera <maderarasto@gmail.com>
2023-08-15 22:59:02 +0200

    Added a generic data structure Queue.

---
+++ src/framework/Core/Queue.ts
@@ -0,0 +1,65 @@
+/**
+ * Represents a data structure in which items are processed in FIFO order.
+ */
+export default class Queue<T> {
+    private items: T[];
+
+    constructor() {
+        this.items = [];
+    }
+
+    /**
+     * Get size of queue.
+     *
+     * @readonly
+     * @type {number}
+     * @memberof Queue
+     */
+    get length(): number {
+        return this.items.length;
+    }
+
+    /**
+     * Check if the queue is empty.
+     * @returns 
+     */
+    isEmpty(): boolean {
+        return this.items.length === 0;
+    }
+
+    /**
+     * Get a first item in queue.
+     * If queue is empty then returns undefined.
+     * 
+     * @returns item or undefined.
+     */
+    peek(): T | undefined {
+        return this.items[0];
+    }
+
+    /**
+     * Push a new item into the queue at its end.
+     * 
+     * @param item new item
+     */
+    enqueue(item: T) {
+        this.items.push(item);
+    }
+
+    /**
+     * Pop first item in the queue and returns it.
+     * If the queue is empty then raises an error.
+     * 
+     * @returns first item.
+     */
+    dequeue(): T {
+        if (this.isEmpty()) {
+            throw new Error('Cannot use an operation dequeue on empty queue!');
+        }
+
+        const item = this.peek() as T;
+        this.items = [...this.items.slice(1)];
+
+        return item;
+    }
+}
