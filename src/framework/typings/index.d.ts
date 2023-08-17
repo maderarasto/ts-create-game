6230ba33c27996e439a26fefb6d258ab3bd0c7fc
Rastislav Madera <maderarasto@gmail.com>
2023-08-17 11:25:16 +0200

    Input and Event Handler #12: add data types of events

--- src/framework/typings/index.d.ts
+++ src/framework/typings/index.d.ts
@@ -7,9 +7,81 @@ namespace Core {
             height: number
         }
     }
+
+    /**
+     * Represents a type event that can occur in event queue.
+     */
+    type EventType = (
+        | 'KeyDown'
+        | 'KeyUp'
+        | 'MouseDown'
+        | 'MouseUp'
+        | 'MouseMove'
+        | 'MouseEnter'
+        | 'MouseLeave'
+        | 'MouseWheel'
+    )
+
+    /**
+     * Represents button on mouse.
+     */
+    type MouseButton = (
+        | 'Left'
+        | 'Middle'
+        | 'Right'
+    )
+
+    /**
+     * Represents status in which can be keyboard keys.
+     */
+    type KeyboardKeyStatus = (
+        | 'Pressed' 
+        | 'Released'
+    )
+
+    /**
+     * Represents an event in event queue.
+     */
+    type Event = {
+        type: EventType
+    } | {
+        type: 'KeyDown'|'KeyUp',
+        code: string
+        key: string
+        alt: boolean
+        ctrl: boolean
+        shift: boolean
+    } | {
+        type: 'MouseMove'|'MouseEnter'|'MouseLeave',
+        x: number
+        y: number
+        alt: boolean
+        ctrl: boolean
+        shift: boolean
+        button: MouseButton
+    } | {
+        type: 'MouseDown'|'MouseUp',
+        x: number
+        y: number
+        alt: boolean
+        ctrl: boolean
+        shift: boolean
+        button: MouseButton
+    } | {
+        type: 'MouseWheel',
+        x: number
+        y: number
+        alt: boolean
+        ctrl: boolean
+        shift: boolean
+        delta: number
+    }
 }
 
 namespace Entities {
+    /**
+     * Represents a category if entities.
+     */
     type Category = (
         | 'Entity'
         | 'Enemy'     
